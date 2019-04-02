e94711fc3e92f0725cdcde2186b3474955f0eac6
wismark <trustworthy.wismark@gmail.com>
2019-04-02 12:27:20 +0300

    fix

--- Cash Machine/Global.asax.cs
+++ Cash Machine/Global.asax.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Web;
+﻿using System.Reflection;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using Autofac.Integration.Mvc;
-//using CM.DiResolver;
 using CM.Services;
 
 namespace Cash_Machine
